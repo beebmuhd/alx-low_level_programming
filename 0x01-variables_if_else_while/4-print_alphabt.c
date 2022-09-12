f61babe617c27ab7ab1ed47d1933202fb17273ec
beebmuhd <habibullahamuhd@gmail.com>
2022-09-11 17:01:30 -0700

    print alpha

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,7 +6,6 @@
  *main - Entry point
  * 
  *Return: Always 0 (Success)
- *
  */
 
 int main(void)
@@ -15,14 +14,11 @@ int main(void)
 
 	e = 'e';
 	q = 'q';
-
 	for (low = 'a'; low <= 'z'; low++)
 	{
 		if (low != e && low != q)
 			putchar(low);
 	}
 	putchar('\n');
-
 	return (0);
-
 }
