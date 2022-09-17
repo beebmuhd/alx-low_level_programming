a4237264ed450bbec7d954c7f93a76d8d3015449
beebmuhd <habibullahamuhd@gmail.com>
2022-09-16 19:06:43 -0700

    numbers

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,27 +1,53 @@
 #include <unistd.h>
 
-#include "holberton.h"
+
+
+#include "main.h"
+
+
+
+
 
 
 
 /**
  *
- *  * _putchar - writes the character c to stdout
+ *  *
  *
- *   * @c: The character to print
+ *   *  * _putchar - writes the character c to stdout
  *
  *    *
  *
- *     * Return: On success 1.
+ *     *   * @c: The character to print
  *
- *      * On error, -1 is returned, and errno is set appropriately.
+ *      *
  *
- *       */
+ *       *    *
+ *
+ *        *
+ *
+ *         *     * Return: On success 1.
+ *
+ *          *
+ *
+ *           *      * On error, -1 is returned, and errno is set appropriately.
+ *
+ *            *
+ *
+ *             *       */
+
+
 
 int _putchar(char c)
 
+
+
 {
 
-		return (write(1, &c, 1));
+
+
+			return (write(1, &c, 1));
+
+
 
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,6 +8,7 @@ int _isupper(int c);
 
 int _isdigit(int c);
 
-
+void print_numbers(void);
+int _putchar(char c);
 
 #endif /*_MAIN_H*/
